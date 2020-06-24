b440a860a0932267babb01e9461fecc020203416
Mikhaila Alvarez <64713956+mikhailaayeisha@users.noreply.github.com>
2020-06-24 15:44:33 -0700

    Add files via upload
    first time doing python!

---
+++ Basic.py
@@ -0,0 +1,28 @@
+for count in range(0,501)
+    print(count) #1 basic
+
+for count in range(5,1000001,5):
+    print(count) #2
+
+for i in range(1,101)
+    if i % 5 == 0:
+        print("Coding")
+    elif i % 10 == 0:
+         print("Dojo")
+    else:
+         print(i) #3
+sum = 0
+for i in range (0,500000):
+    if i % 2 == 1:
+    sum += i
+    print(sum)  #4
+
+for count in range (2018,0,-4):
+    print(count) #5
+
+def flexibleCountdown(highNum,lowNum,mult):
+    for i in range (lowNum, highNum):
+        if i % mult == 0:
+            print(i) #6
+
+
